2a7dae203994e8b8cbd77d378c5a396f8516040b
Jonas Schubert <guepardoapps@gmail.com>
2019-10-10 13:14:34 +0200

    Update code.js

--- code.js
+++ code.js
@@ -6,34 +6,34 @@ Puck.on('mag', function(values) {
 });
 */
 
-//Code to read out light sensor
+// Code to read out light sensor
 /*
 Puck.light();
 */
 
-//Code to read out temperature
+// Code to read out temperature
 /*
 E.getTemperature();
 */
 
-//Code to read out battery level
+// Code to read out battery level
 /*
 Puck.getBatteryPercentage();
 */
 
-//Code for NFC - Near field communication
+// Code for NFC - Near field communication
 /*
 NRF.nfcURL("http://192.168.178.25");
 */
 
-//Code for iBeacons
+// Code for iBeacons
 /*
 require("ble_ibeacon").advertise({
   uuid: [0, 2, 0, 1, 1, 9, 9, 0, 0, 2, 0, 1, 1, 9, 9, 0]
 });
 */
 
-//Another code for Beacons (Eddystone)
+// Another code for Beacons (Eddystone)
 /*
 NRF.setAdvertising([
   require("ble_ibeacon").get({
@@ -43,21 +43,27 @@ NRF.setAdvertising([
   ], {interval:250});
 */
 
-//Broadcast LucaHome IP
+// Broadcast LucaHome IP
 // NRF.nfcURL("http://192.168.178.25");
 
-//Places of the PuckJS in my flat
-//var places = ["Hall", "Bath", "Sleeping_Room", "Living_Room", "Kitchen"];
+// Places of the PuckJS in my flat
+// const places = ["Hallway", "Bath", "Bedroom", "Living Room", "Kitchen"];
 
-//Advertise place, temperature, light, battery percentage and mag values via NRF
+// Advertise place, temperature, light, battery percentage and mag values via NRF
 setInterval(function() {
-  //var magValue = Puck.mag();
-  //var magString = "x: " + String(magValue.x) + "&y:" + String(magValue.y) + "&z:" + String(magValue.z);
-  // Max advertising values are four (!?)
+  // const magValue = Puck.mag();
+  // const magString = "x: " + String(magValue.x) + "&y:" + String(magValue.y) + "&z:" + String(magValue.z);
+  // TODO: Question: Max advertising values are four!?
+
+  const battery = Puck.getBatteryPercentage();
+
+  if(battery < 10)
+    LED1.write(true);
+
   NRF.setAdvertising({
     0x1809: [Math.round(E.getTemperature())],
     0x1819: [Puck.light() * 100],
-    0x1829: [Puck.getBatteryPercentage()]//,
-    //0x1839: [magString]
+    0x1829: [battery] // ,
+    // 0x1839: [magString]
   });
-}, 15000);
+}, 10000);
