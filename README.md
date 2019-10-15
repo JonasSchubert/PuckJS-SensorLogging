0172b2142518f870e30f7ee6e36ba883fce58e19
Jonas Schubert <guepardoapps@gmail.com>
2019-10-15 22:05:24 +0200

    sending namespace & instance as well as proper battery id

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![Donate: PayPal](https://img.shields.io/badge/paypal-donate-blue.svg)](https://www.paypal.me/GuepardoApps)
 
 [![Build](https://img.shields.io/badge/build-success-green.svg)](./code.js)
-[![Version](https://img.shields.io/badge/version-v0.1.0.191010-blue.svg)](./code.js)
+[![Version](https://img.shields.io/badge/version-v0.1.1.191015-blue.svg)](./code.js)
 [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](http://makeapullrequest.com)
 
 ## Home Assistant
--- code.js
+++ code.js
@@ -49,21 +49,41 @@ NRF.setAdvertising([
 // Places of the PuckJS in my flat
 // const places = ["Hallway", "Bath", "Bedroom", "Living Room", "Kitchen"];
 
+// Instances
+/*
+const instances = [
+  "0x53, 0x69, 0x6e, 0x74, 0x68, 0x73", // Hallway      B8E
+  "0x53, 0x69, 0x6e, 0x74, 0x68, 0x72", // Bath         E4D
+  "0x53, 0x69, 0x6e, 0x74, 0x68, 0x71", // Bedroom      BF7
+  "0x53, 0x69, 0x6e, 0x74, 0x68, 0x75", // Living Room  448
+  "0x53, 0x69, 0x6e, 0x74, 0x68, 0x74"  // Kitchen      FEE
+];
+*/
+
 // Advertise place, temperature, light, battery percentage and mag values via NRF
-setInterval(function() {
+setInterval(() => {
   // const magValue = Puck.mag();
   // const magString = "x: " + String(magValue.x) + "&y:" + String(magValue.y) + "&z:" + String(magValue.z);
   // TODO: Question: Max advertising values are four!?
 
   const battery = Puck.getBatteryPercentage();
 
-  if(battery < 10)
+  if (battery < 10) {
     LED1.write(true);
+  }
 
-  NRF.setAdvertising({
-    0x1809: [Math.round(E.getTemperature())],
-    0x1819: [Puck.light() * 100],
-    0x1829: [battery] // ,
-    // 0x1839: [magString]
-  });
-}, 10000);
+  // https://www.bluetooth.com/specifications/gatt/services/
+  NRF.setAdvertising(
+    [
+      require("ble_eddystone_uid").get(
+        [0x4c, 0x75, 0x63, 0x61, 0x48, 0x6f, 0x6d, 0x65, 0x32, 0x30], // namespace
+        [0x53, 0x69, 0x6e, 0x74, 0x68, 0x73]                          // instance
+      ),
+      {
+        0x1809: [Math.round(E.getTemperature())], // Health Thermometer
+        0x180F: [battery],                        // Battery Service
+        0x181A: [Puck.light() * 100],             // Environmental Sensing
+      }
+    ]
+  );
+}, 1000);
