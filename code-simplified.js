c270d7d01c659caecf60b07332aada63fa7da5dc
Jonas Schubert <jonas.schubert.projects@web.de>
2020-05-31 08:29:21 +0200

    [improvement] resets LED, increases timeout

--- code-simplified.js
+++ code-simplified.js
@@ -1,14 +1,15 @@
 setInterval(() => {
-    const battery = Puck.getBatteryPercentage();
-  
-    if (battery < 10) {
-      LED1.write(true);
-    }
-  
-    NRF.setAdvertising({
-      0x1809: [Math.round(E.getTemperature())],
-      0x2A19: [battery],
-      0x3232: [Number(Puck.light()) * 0x64]
-    });
-  }, 15000);
-  
+  const battery = Puck.getBatteryPercentage();
+
+  if (battery < 10) {
+    LED1.write(true);
+  } else {
+    LED1.write(false);
+  }
+
+  NRF.setAdvertising({
+    0x1809: [Math.round(E.getTemperature())],
+    0x2A19: [battery],
+    0x3232: [Number(Puck.light()) * 0x64]
+  });
+}, 30000);
